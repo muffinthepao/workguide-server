47e766fcedab0c0225dbe7bd0dd147d37f76d2b9
Mervin Poh <mervpoh@gmail.com>
2022-10-18 19:43:33 +0800

    fix: dotenv variables for production

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
-{
+require("dotenv").config();
+
+module.exports = {
   "development": {
     "database": "workguide_development",
     "host": "127.0.0.1",
@@ -10,10 +12,10 @@
     "dialect": "postgres"
   },
   "production": {
-    "username": "lupfymhg",
-    "password": "jmylUZPlfqD3QzGQgP4XBKyAcNG2wGmT",
-    "database": "lupfymhg",
-    "host": "topsy.db.elephantsql.com",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
 }
