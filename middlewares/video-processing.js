9f810f6b51f1e0de05910fd62c8573711232b0f2
Mervin Poh <mervpoh@gmail.com>
2022-10-23 23:44:52 +0800

    fix: still trying to fix

--- middlewares/video-processing.js
+++ middlewares/video-processing.js
@@ -9,11 +9,17 @@ const imagekit = new ImageKit({
 
 const videoProcessingMethods = {
   mergeVideos: async (req, res, next) => {
-    console.log(" 7 -- reached merge videos middleware")
+    console.log(" 7 -- reached merge videos middleware");
     console.log("req.body: ", req.body);
     console.log("req.files: ", req.files);
 
-    const videoPartDurations = JSON.parse(req.body.blobDurations);
+    const stringToConvert = req.body.blobDurations;
+    const splitStringToArray = stringToConvert.split(",");
+    const blobDurations = splitStringToArray.map((string) =>
+      parseFloat(string)
+    );
+
+    const videoPartDurations = blobDurations;
     const imagekitURLs = req.files;
     let shotstackArray = [];
 
@@ -33,7 +39,7 @@ const videoProcessingMethods = {
       });
 
       startingTime += videoPartDurations[i];
-      console.log("startingTime: ", startingTime)
+      console.log("startingTime: ", startingTime);
     }
 
     try {
@@ -47,7 +53,8 @@ const videoProcessingMethods = {
             format: "mp4",
             resolution: "sd",
           },
-          callback: "https://workguide-server.herokuapp.com/api/v1/questions/shortstack-callback"
+          callback:
+            "https://workguide-server.herokuapp.com/api/v1/questions/shortstack-callback",
         },
         {
           headers: {
@@ -56,23 +63,28 @@ const videoProcessingMethods = {
           },
         }
       );
-  
-      console.log(dataToSend)
-      if (dataToSend.data.success === true) {
-        req.body.shotstackId = dataToSend.data.response.id
-        res.status(200).json({ success: "we are merging your video answer! Please checkback when in a few minutes"});
-        console.log(" 8 -- shotstack queue stuccess")
-        return next()
-      } else {
-        console.log(data)
-        res.status(500).json({message: "unable to merge video at this time. Please try again later"})
-      }
-      
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({message: "unable to merge video at this time. Please try again later"})
-    }
 
+      console.log(dataToSend);
+      if (dataToSend.data.success === true) {
+        req.body.shotstackId = dataToSend.data.response.id;
+        res.status(200).json({
+          success:
+            "we are merging your video answer! Please checkback when in a few minutes",
+        });
+        console.log(" 8 -- shotstack queue stuccess");
+        return next();
+      } else {
+        console.log(data);
+        res.status(500).json({
+          message: "unable to merge video at this time. Please try again later",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "unable to merge video at this time. Please try again later",
+      });
+    }
   },
   uploadVideo: async (req, res, next) => {
     console.log("1-have req.files");
@@ -118,108 +130,109 @@ const videoProcessingMethods = {
     return next();
   },
   uploadMultipleVideos: async (req, res, next) => {
-    console.log(" 1 -- reached upload videos middleware")
+    console.log(" 1 -- reached upload videos middleware");
     // console.log("1 -- req.body", req.body);
     // console.log("1 -- req.body", req.body.blobDurations);
     // console.log("1 -- req.body", typeof req.body.blobDurations);
     // console.log("1 -- req.files", req.files);
 
     if (req.files) {
-    // multer midleware allows the req.file to come through
-    // console.log(" 1-have req.files");
-    // console.log("2-req.files--->");
+      // multer midleware allows the req.file to come through
+      // console.log(" 1-have req.files");
+      // console.log("2-req.files--->");
 
-    const promises = req.files.map(async (file) => { //async waits for promise1
-      return await new Promise((resolve, reject) => { // this is promise1
-        const response = imagekit.upload(
-          {
-            file: file.buffer,
-            fileName: "blob.mp4", //required
-            folder: "workguide",
-          },
-          function (err, response) {
-            if (err) {
-              reject(err);
-              console.log(err);
-              return res.status(500).json({
-                status: "failed",
-                message:
-                  "An error occured during file upload. Please try again.",
-              });
-            } else {
-              resolve(response);
-              // console.log("3 -", response);
+      const promises = req.files.map(async (file) => {
+        //async waits for promise1
+        return await new Promise((resolve, reject) => {
+          // this is promise1
+          const response = imagekit.upload(
+            {
+              file: file.buffer,
+              fileName: "blob.mp4", //required
+              folder: "workguide",
+            },
+            function (err, response) {
+              if (err) {
+                reject(err);
+                console.log(err);
+                return res.status(500).json({
+                  status: "failed",
+                  message:
+                    "An error occured during file upload. Please try again.",
+                });
+              } else {
+                resolve(response);
+                // console.log("3 -", response);
+              }
             }
-          }
-        );
-        return response //returns response to promise1
+          );
+          return response; //returns response to promise1
+        });
       });
-    });
-    const fileUrls = await Promise.all(promises); //returns a single promise of promises
-    console.log(" 2 -- files rendered by Imagekit")
+      const fileUrls = await Promise.all(promises); //returns a single promise of promises
+      console.log(" 2 -- files rendered by Imagekit");
 
-    // let fileUrls = [
-    //   {
-    //     fileId: "6353fc2ebf51c1dc80e58749",
-    //     name: "blob_QV03qPJNJ.mp4",
-    //     size: 304703,
-    //     versionInfo: { id: "6353fc2ebf51c1dc80e58749", name: "Version 1" },
-    //     filePath: "/workguide/blob_QV03qPJNJ.mp4",
-    //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_QV03qPJNJ.mp4",
-    //     fileType: "non-image",
-    //     AITags: null,
-    //   },
-    //   {
-    //     fileId: "6353fc2ebf51c1dc80e587a0",
-    //     name: "blob_dNa4mDRSOu.mp4",
-    //     size: 218548,
-    //     versionInfo: { id: "6353fc2ebf51c1dc80e587a0", name: "Version 1" },
-    //     filePath: "/workguide/blob_dNa4mDRSOu.mp4",
-    //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_dNa4mDRSOu.mp4",
-    //     fileType: "non-image",
-    //     AITags: null,
-    //   },
-    //   {
-    //     fileId: "6353fc2ebf51c1dc80e587f6",
-    //     name: "blob_XQl9jVBxLI.mp4",
-    //     size: 298674,
-    //     versionInfo: { id: "6353fc2ebf51c1dc80e587f6", name: "Version 1" },
-    //     filePath: "/workguide/blob_XQl9jVBxLI.mp4",
-    //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_XQl9jVBxLI.mp4",
-    //     fileType: "non-image",
-    //     AITags: null,
-    //   },
-    //   {
-    //     fileId: "6353fc2ebf51c1dc80e587c4",
-    //     name: "blob_a29mA_jPP.mp4",
-    //     size: 199808,
-    //     versionInfo: { id: "6353fc2ebf51c1dc80e587c4", name: "Version 1" },
-    //     filePath: "/workguide/blob_a29mA_jPP.mp4",
-    //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_a29mA_jPP.mp4",
-    //     fileType: "non-image",
-    //     AITags: null,
-    //   },
-    // ];
-    let imageKitIds = [];
-    let imageKitUrls = [];
+      // let fileUrls = [
+      //   {
+      //     fileId: "6353fc2ebf51c1dc80e58749",
+      //     name: "blob_QV03qPJNJ.mp4",
+      //     size: 304703,
+      //     versionInfo: { id: "6353fc2ebf51c1dc80e58749", name: "Version 1" },
+      //     filePath: "/workguide/blob_QV03qPJNJ.mp4",
+      //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_QV03qPJNJ.mp4",
+      //     fileType: "non-image",
+      //     AITags: null,
+      //   },
+      //   {
+      //     fileId: "6353fc2ebf51c1dc80e587a0",
+      //     name: "blob_dNa4mDRSOu.mp4",
+      //     size: 218548,
+      //     versionInfo: { id: "6353fc2ebf51c1dc80e587a0", name: "Version 1" },
+      //     filePath: "/workguide/blob_dNa4mDRSOu.mp4",
+      //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_dNa4mDRSOu.mp4",
+      //     fileType: "non-image",
+      //     AITags: null,
+      //   },
+      //   {
+      //     fileId: "6353fc2ebf51c1dc80e587f6",
+      //     name: "blob_XQl9jVBxLI.mp4",
+      //     size: 298674,
+      //     versionInfo: { id: "6353fc2ebf51c1dc80e587f6", name: "Version 1" },
+      //     filePath: "/workguide/blob_XQl9jVBxLI.mp4",
+      //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_XQl9jVBxLI.mp4",
+      //     fileType: "non-image",
+      //     AITags: null,
+      //   },
+      //   {
+      //     fileId: "6353fc2ebf51c1dc80e587c4",
+      //     name: "blob_a29mA_jPP.mp4",
+      //     size: 199808,
+      //     versionInfo: { id: "6353fc2ebf51c1dc80e587c4", name: "Version 1" },
+      //     filePath: "/workguide/blob_a29mA_jPP.mp4",
+      //     url: "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_a29mA_jPP.mp4",
+      //     fileType: "non-image",
+      //     AITags: null,
+      //   },
+      // ];
+      let imageKitIds = [];
+      let imageKitUrls = [];
 
-    fileUrls.forEach((file) => {
-      imageKitIds.push(file.fileId);
-      imageKitUrls.push(file.url);
-    });
-    req.body.imageKitIds = imageKitIds;
-    // console.log("req.body", req.body)
-    // console.log("videoIds: ", videoIds)
-    // console.log("videoUrls: ", videoUrls)
+      fileUrls.forEach((file) => {
+        imageKitIds.push(file.fileId);
+        imageKitUrls.push(file.url);
+      });
+      req.body.imageKitIds = imageKitIds;
+      // console.log("req.body", req.body)
+      // console.log("videoIds: ", videoIds)
+      // console.log("videoUrls: ", videoUrls)
 
-    // console.log("2 -- req.body", req.body);
-    // console.log("2 -- req.files", req.files);
+      // console.log("2 -- req.body", req.body);
+      // console.log("2 -- req.files", req.files);
 
-    req.files = imageKitUrls;
-    console.log(" 3 -- Imagekit urls and ids attached to req")
+      req.files = imageKitUrls;
+      console.log(" 3 -- Imagekit urls and ids attached to req");
 
-    return next();
-
+      return next();
     } else {
       console.log("no req.file");
       return res.status(500).json({
--- test.js
+++ test.js
@@ -1,65 +1,75 @@
-const urls = [
-  "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_QV03qPJNJ.mp4",
-  "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_dNa4mDRSOu.mp4",
-  "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_XQl9jVBxLI.mp4",
-  "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_a29mA_jPP.mp4",
-];
+// const urls = [
+//   "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_QV03qPJNJ.mp4",
+//   "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_dNa4mDRSOu.mp4",
+//   "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_XQl9jVBxLI.mp4",
+//   "https://ik.imagekit.io/7m4pg6sx4/workguide/blob_a29mA_jPP.mp4",
+// ];
 
-const duration = [4, 2, 3, 2];
-let array = [];
+// const duration = [4, 2, 3, 2];
+// let array = [];
 
-let startingTime = 0;
-for (let i = 0; i < urls.length; i++) {
-  array.push({
-    clips: [
-      {
-        asset: {
-          type: "video",
-          src: urls[i],
-        },
-        start: startingTime,
-        length: duration[i],
-      },
-    ],
-  });
-
-  startingTime += duration[i]
-}
-
-// let array = urls.map((url, index) => {
-//   for (let i=0; i <= index; i++) {
-
-//   }
-
-//   return {
+// let startingTime = 0;
+// for (let i = 0; i < urls.length; i++) {
+//   array.push({
 //     clips: [
 //       {
 //         asset: {
 //           type: "video",
-//           src: url,
+//           src: urls[i],
 //         },
-//         start: 0,
-//         length: duration[index],
+//         start: startingTime,
+//         length: duration[i],
 //       },
 //     ],
-//   };
-//   // } else {
-//   //   return {
-//   //     "clips": [
-//   //       {
-//   //         "asset": {
-//   //           "type": "video",
-//   //           "src": url
-//   //         },
-//   //         "start": 0,
-//   //         "length": duration[index]
-//   //       }
-//   //     ]
-//   //   }
-//   // }
-// });
+//   });
 
-console.log(array[0].clips);
-console.log(array[1].clips);
-console.log(array[2].clips);
-console.log(array[3].clips);
+//   startingTime += duration[i]
+// }
+
+// // let array = urls.map((url, index) => {
+// //   for (let i=0; i <= index; i++) {
+
+// //   }
+
+// //   return {
+// //     clips: [
+// //       {
+// //         asset: {
+// //           type: "video",
+// //           src: url,
+// //         },
+// //         start: 0,
+// //         length: duration[index],
+// //       },
+// //     ],
+// //   };
+// //   // } else {
+// //   //   return {
+// //   //     "clips": [
+// //   //       {
+// //   //         "asset": {
+// //   //           "type": "video",
+// //   //           "src": url
+// //   //         },
+// //   //         "start": 0,
+// //   //         "length": duration[index]
+// //   //       }
+// //   //     ]
+// //   //   }
+// //   // }
+// // });
+
+// console.log(array[0].clips);
+// console.log(array[1].clips);
+// console.log(array[2].clips);
+// console.log(array[3].clips);
+
+// let string = '8.213,5.511,5.808,7.059'
+
+// let noString = string.split(",")
+
+// console.log(noString)
+
+// const blob = noString.map(x => parseFloat(x))
+
+// console.log(blob)
