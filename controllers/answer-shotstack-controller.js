647216b67742a3e546a5b846d195a7b493b224f2
Mervin Poh <mervpoh@gmail.com>
2022-10-24 18:35:14 +0800

    chore: separeated out answer routes (shotstack, url, generic) to separate controllers added user authorization when editing URL answers

---
+++ controllers/answer-shotstack-controller.js
@@ -0,0 +1,103 @@
+// const bcrypt = require("bcrypt");
+
+const answerValidator = require("../joi-validators/answer");
+const db = require("../models");
+
+module.exports = {
+  createShotStackAnswer: async (req, res, next) => {
+    console.log(" 4 -- reach create Answer step");
+
+    // console.log("answer created");
+    // console.log("4 -- req.body: ", req.body);
+    // console.log("4 --req.files: ", req.files);
+
+    // joi validations for answer inputs
+    // let errorObject = {};
+
+    // const answerValidationResults =
+    //   answerValidator.createAnswerValidator.validate(req.body, {
+    //     abortEarly: false,
+    //   });
+
+    // if (answerValidationResults.error) {
+    //   const validationError = answerValidationResults.error.details;
+
+    //   validationError.forEach((error) => {
+    //     errorObject[error.context.key] = error.message;
+    //   });
+    //   console.log(errorObject)
+    //   return res.status(400).json(errorObject);
+    // }
+
+    // let validatedAnswer = { ...answerValidationResults.value };
+
+    let allImageKitVideoUrls = JSON.stringify(req.files);
+    let allImageKitVideoIds = JSON.stringify(req.body.imageKitIds);
+
+    try {
+      const createdAnswer = await db.answer.create({
+        imageKitUrls: allImageKitVideoUrls,
+        imageKitIds: allImageKitVideoIds,
+        userId: req.body.userId,
+        questionId: req.body.questionId,
+      });
+
+      console.log("createdAnswer: ", createdAnswer);
+
+      console.log(" 5 -- record created in db");
+      req.body.answerId = createdAnswer.id;
+      console.log(" 6 -- db.answer.id appended to req.body");
+
+      return next();
+      // res.status(201).json({ success: "answer created" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "failed to create answer" });
+    }
+  },
+  insertShotstackIdIntoDB: async (req, res, next) => {
+    console.log(" 9 -- start shotstackId insertion");
+    const shotstackId = req.body.shotstackId;
+    const answerId = req.body.answerId;
+
+    try {
+      const answerUpdated = await db.answer.update(
+        { shotstackId },
+        {
+          where: {
+            id: answerId,
+          },
+        }
+      );
+      console.log("answerUpdated", answerUpdated);
+      console.log(" 10 -- successful shotstackId insertion");
+      return next();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "unable to update shotstackId" });
+    }
+  },
+  insertShotstackUrlIntoDB: async (req, res) => {
+    console.log(" 11 -- start answerUrl insertion");
+    console.log("11 -- req.body: ,", req.body);
+    const shotstackId = req.body.id;
+    const shotstackUrl = req.body.url;
+
+    try {
+      await db.answer.update(
+        { answerUrl: shotstackUrl, status: "completed" },
+        {
+          where: {
+            shotstackId,
+          },
+        }
+      );
+
+      console.log(" 12 -- successful shotstackUrl insertion");
+      res.status(201).json({ message: "video answer created" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "unable to update shotstackId" });
+    }
+  },
+};
--- router/question-routes.js
+++ router/question-routes.js
@@ -4,7 +4,9 @@ const multer = require("multer")
 const upload = multer()
 
 const questionsController = require('../controllers/question-controller')
-const answersController = require('../controllers/answer-controller')
+const answersShotStackController = require('../controllers/answer-shotstack-controller')
+const answersUrlController = require('../controllers/answer-url-controller')
+const answersGenericController = require('../controllers/answer-generic-controller')
 const videoUploadsMiddleware = require ('../middlewares/video-processing')
 
 const router = express.Router()
@@ -17,29 +19,27 @@ router.put('/:questionId/update', questionsController.updateQuestion)
 router.delete('/:questionId/delete', questionsController.deleteQuestion)
 
 
+// **** SHOTSTACK ANSWER ROUTES **** //
+// create, update, delete answer
+// router.post('/:questionId/answers', upload.single("file"), videoUploadsMiddleware.uploadVideo, answersShotStackController.create)
 
-// create, list, show, update, delete answer
-// router.post('/:questionId/answers', upload.single("file"), videoUploadsMiddleware.uploadVideo, answersController.create)
+router.post('/:questionId/answers/process-multi', upload.any("files"), videoUploadsMiddleware.uploadMultipleVideos, answersShotStackController.createShotStackAnswer, videoUploadsMiddleware.mergeVideos, answersShotStackController.insertShotstackIdIntoDB)
 
-// create using videobooth or multi file upload
-router.post('/:questionId/answers/process-multi', upload.any("files"), videoUploadsMiddleware.uploadMultipleVideos, answersController.createAnswerUsingFiles, videoUploadsMiddleware.mergeVideos, answersController.insertShotstackIdIntoDB)
 
-//create using URL
-router.post('/:questionId/answers/url-interstion', answersController.createAnswerUsingUrl)
 
-// router.post('/:questionId/answers/create', videoUploadsMiddleware.uploadMultipleVideos, answersController.createAnswer, videoUploadsMiddleware.mergeVideos, answersController.insertShotstackIdIntoDB)
-router.get('/:questionId/answers', answersController.listAnswers)
+// router.post('/:questionId/answers/create', videoUploadsMiddleware.uploadMultipleVideos, answersShotStackController.createAnswer, videoUploadsMiddleware.mergeVideos, answersShotStackController.insertShotstackIdIntoDB)
+
+
+// router.delete('/:questionId/answers/delete-shotstack')
+router.post('/shortstack-callback', answersShotStackController.insertShotstackUrlIntoDB)
+
+// **** URL ANSWER ROUTES **** //
+// create, update, delete answer
+router.post('/:questionId/answers/url-interstion', answersUrlController.createUrlAnswer)
+router.patch('/:questionId/answers/:answerId/url-insertion', answersUrlController.updateUrlAnswer)
+
+//GENERIC ANSWER ROUTES
+router.get('/:questionId/answers', answersGenericController.listAnswers)
 // router.get('/:questionId/answers/:answerId', answersController.showAnswer)
-// router.delete('/:questionId/answers/delete')
-router.post('/shortstack-callback', answersController.insertShotstackUrlIntoDB)
-
-
-// multer
-// imagekit
-// create record in db status is pending
-// shotstack + "callback". normally there is a callback url but currently dont have
-// update record in db
-// set interval is perpetrual until i clear it after rendering is complete
-// google alternatives to set interval
 
 module.exports = router;
